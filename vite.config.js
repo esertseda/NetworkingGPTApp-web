d3f5bfdc707fd8445f2b7066be65dcf1b866e753
fatiihhko <esertaseda@gmail.com>
2025-08-25 10:34:48 +0300

    Update web design 7

--- vite.config.js
+++ vite.config.js
@@ -9,5 +9,9 @@ export default defineConfig({
         invite: 'invite.html'
       }
     }
+  },
+  define: {
+    'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(process.env.VITE_SUPABASE_URL),
+    'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(process.env.VITE_SUPABASE_ANON_KEY)
   }
 })
